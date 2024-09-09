f4a84483d71e80abf314ea54ed7cbaff5c9a083b
Tejasvi68 <tejasvipanoli@gmail.com>
2024-09-09 10:57:00 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,14 @@
+package student;
+public class s1
+{
+    public void sdata()
+    {
+        String sname="tejasvi";
+        int rollno=121;
+        int age=18;
+        System.out.println("student name="+sname);
+        System.out.println("student rollno="+rollno);
+        System.out.println("student age="+age);
+    
+    }
+}
